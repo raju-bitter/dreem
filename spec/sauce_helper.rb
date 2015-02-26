bc7403d9670f876a8568e06a6759226345d82d76
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2015-02-26 14:36:04 -0800

    wip

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -6,9 +6,9 @@ Capybara.default_driver = :sauce
 
 # Set up configuration
 Sauce.config do |c|
-  c[:start_tunnel] = true,
-  c[:sauce_connect_4_executable] = './spec/sc-4.3.6-osx/bin/sc',
-  c[:connect_options] = {:logfile => 'sauceconnect_rspec.log'},
+  c[:start_tunnel] = false,
+  #c[:sauce_connect_4_executable] = './spec/sc-4.3.6-osx/bin/sc',
+  #c[:connect_options] = {:logfile => 'sauceconnect_rspec.log'},
   c[:browsers] = [
       #["Windows 8.1", "Firefox", nil]#,
       #["Windows 8.1", "Chrome", nil],
