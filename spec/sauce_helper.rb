4beb29772bce856703b55dc66e6ca0e74e0399a8
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2015-02-26 14:19:28 -0800

    rspecs now running sucessfully on sauce

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -6,11 +6,14 @@ Capybara.default_driver = :sauce
 
 # Set up configuration
 Sauce.config do |c|
+  c[:start_tunnel] = true,
+  c[:sauce_connect_4_executable] = './spec/sc-4.3.6-osx/bin/sc',
+  c[:connect_options] = {:logfile => 'sauceconnect_rspec.log'},
   c[:browsers] = [
-      ["Windows 8.1", "Firefox", nil]#,
+      #["Windows 8.1", "Firefox", nil]#,
       #["Windows 8.1", "Chrome", nil],
       #["OS X 10.10", "Firefox", nil],
-      #["OS X 10.10", "Chrome", nil],
+      ["OS X 10.10", "Chrome", nil]
       #["Linux", "Chrome", nil],
       #["Linux", "Firefox", nil],
       #['OS X 10.9', 'iPhone', '8.1'],
